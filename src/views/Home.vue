830320222f9c9f2b4b7ee6a9eed9118696c610e3
anu-oluwapo <abolarin.aanu@gmail.com>
2021-06-28 20:38:45 +0100

    Minor Fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -352,7 +352,11 @@ export default {
     
       
 </script>
-
+<style>
+h4{
+display: block
+}
+</style>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap");
 
